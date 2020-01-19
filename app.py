de2f56201c57b98fb802c815254decc17d2c436f
Max Harris <mharris7190@gmail.com>
2020-01-19 17:22:46 -0600

    environment variable for port

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, jsonify, request
 from nltk.corpus import gutenberg
 from nltk.tokenize import sent_tokenize
 import nltk
+import os
 nltk.download('gutenberg')
 nltk.download('punkt')
 
+port = int(os.environ.get("PORT", 5000))
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
@@ -16,4 +18,4 @@ def hello():
   return jsonify({'tok': tok[start:end]})
 
 if __name__ == '__main__':
-  app.run(host="0.0.0.0", port=5000, debug=True)
+  app.run(host="0.0.0.0", port=port)
